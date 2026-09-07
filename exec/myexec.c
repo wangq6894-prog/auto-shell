f5fc39a34cb71177a8da9b22e871de2869b0926b
wangq6894-prog <wangq6894@gmail.com>
2026-09-07 17:56:07 +0800

    myexec

--- exec/myexec.c
+++ exec/myexec.c
@@ -11,15 +11,20 @@ int main()
     pid_t id = fork();
     if(id == 0)
     {
+        sleep(2);
+        printf("子进程运行中....\n");
         // 执行另一个程序的代码
-        execl("/usr/bin/ls", "ls", "-a", "-l", NULL); //程序替换函数
-        exit(0);
+        //execl("/usr/bin/ls", "ls", "-a", "-l", NULL); //程序替换函数
+        //execlp("ls","ls","-a","-l",NULL);
+        char* argv[] = {"ls","-a","-l",NULL};
+        execv("/usr/bin/ls",argv);
+        exit(1);
     }
-    wait(NULL);
-    printf("我的代码运行中....\n");
-    printf("我的代码运行中....\n");
-    printf("我的代码运行中....\n");
-    printf("我的代码运行中....\n");
-    printf("我的代码运行中....\n");
+    //wait(NULL);
+    int status = 0;
+    pid_t rid = waitpid(id, &status, 0);
+    if(rid >0){
+        printf("wait success,exit code :%d\n", WEXITSTATUS(status));
+    }
     return 0;
 }
